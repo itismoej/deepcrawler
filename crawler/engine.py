3784b08d2f258e224d2ffccef02da6f599544928
Mohammad Jafari <m.jafari9877@gmail.com>
2021-01-01 16:46:59 +0330

    make progress percents more beleivable

--- crawler/engine.py
+++ crawler/engine.py
@@ -2,9 +2,11 @@ import asyncio
 import json
 import os
 from concurrent.futures.thread import ThreadPoolExecutor
+from random import randint
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import WebDriverException
 
 from config import settings
 
@@ -22,22 +24,27 @@ def scraper(url: str, site_id: str, consumer, event_loop):
             'progress': percent
         })), loop=event_loop)
 
-    browser = webdriver.Chrome(
-        executable_path=settings.BASE_DIR / 'chromedriver',
-        chrome_options=options,
-    )
+    try:
+        send_progress(randint(23, 35))
+        browser = webdriver.Chrome(
+            executable_path=settings.BASE_DIR / 'chromedriver',
+            chrome_options=options,
+        )
+        send_progress(randint(40, 47))
+        browser.get(url)
+        send_progress(randint(51, 59))
+        html = browser.find_element_by_tag_name('html').get_attribute('outerHTML')
+        send_progress(randint(63, 79))
+        filename = settings.BASE_DIR / 'media' / consumer.crawl_id / f'{site_id}.html'
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        send_progress(randint(82, 93))
+        with open(filename, 'w') as f:
+            f.write(html)
+        send_progress(100)
+        consumer.tasks_status[site_id] = True
+    except WebDriverException:
+        send_progress(-1)
 
-    send_progress(50)
-    browser.get(url)
-    send_progress(70)
-    html = browser.find_element_by_tag_name('html').get_attribute('outerHTML')
-    send_progress(85)
-    filename = settings.BASE_DIR / 'media' / consumer.crawl_id / f'{site_id}.html'
-    os.makedirs(os.path.dirname(filename), exist_ok=True)
-    with open(filename, 'w') as f:
-        f.write(html)
-    send_progress(100)
-    consumer.tasks_status[site_id] = True
     if all(consumer.tasks_status.values()):
         asyncio.ensure_future(consumer.send(text_data=json.dumps({
             'id_done': True,
@@ -52,7 +59,7 @@ def run_engine(initial_links: list[dict], consumer):
         url = site['link']
         asyncio.ensure_future(consumer.send(text_data=json.dumps({
             'site_id': site_id,
-            'progress': 25,
+            'progress': randint(7, 19),
         })), loop=event_loop)
         event_loop.run_in_executor(executor, scraper, url, site_id, consumer, event_loop)
 
