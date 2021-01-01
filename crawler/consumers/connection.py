1fae2d4084dd1df98797a0e194e2be653529ac68
Mohammad Jafari <m.jafari9877@gmail.com>
2021-01-01 22:33:09 +0330

    saving all childrens recursively

--- crawler/consumers/connection.py
+++ crawler/consumers/connection.py
@@ -1,6 +1,7 @@
 import json
 from uuid import uuid4
 
+from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 from crawler.engine import run_engine
@@ -15,13 +16,10 @@ class ConnectionConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
         await self.accept()
-        self.crawl_id = uuid4().hex[8:12]
+        self.crawl_id = uuid4().hex
 
     async def receive(self, text_data=None, bytes_data=None):
         data: dict = json.loads(text_data)
         if 'initial_links' in data:
-            initial_links = data['initial_links']
-            self.tasks_status = {
-                site['id']: False for site in initial_links
-            }
-            run_engine(initial_links, self)
+            run_engine(data, self)
+            # await sync_to_async(run_engine)(data, self)
--- crawler/engine.py
+++ crawler/engine.py
@@ -2,11 +2,12 @@ import asyncio
 import json
 import os
 from concurrent.futures.thread import ThreadPoolExecutor
-from random import randint
+from pathlib import Path
 
+from qaes import aes
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.chrome.options import Options
 
 from config import settings
 
@@ -17,33 +18,16 @@ options.page_load_strategy = 'normal'
 options.headless = True
 
 
-def scraper(url: str, site_id: str, consumer, event_loop):
-    def send_progress(percent: int) -> None:
-        asyncio.ensure_future(consumer.send(text_data=json.dumps({
-            'site_id': site_id,
-            'progress': percent
-        })), loop=event_loop)
-
+def scraper(depth: int, url: str, consumer, event_loop):
     try:
-        send_progress(randint(23, 35))
         browser = webdriver.Chrome(
             executable_path=settings.BASE_DIR / 'chromedriver',
             chrome_options=options,
         )
-        send_progress(randint(40, 47))
-        browser.get(url)
-        send_progress(randint(51, 59))
-        html = browser.find_element_by_tag_name('html').get_attribute('outerHTML')
-        send_progress(randint(63, 79))
-        filename = settings.BASE_DIR / 'media' / consumer.crawl_id / f'{site_id}.html'
-        os.makedirs(os.path.dirname(filename), exist_ok=True)
-        send_progress(randint(82, 93))
-        with open(filename, 'w') as f:
-            f.write(html)
-        send_progress(100)
-        consumer.tasks_status[site_id] = True
-    except WebDriverException:
-        send_progress(-1)
+        directory = settings.BASE_DIR / 'media' / consumer.crawl_id
+        scrape_url(browser, consumer, depth, 0, url, directory)
+    except WebDriverException as e:
+        print('web driver exception:', e)
 
     if all(consumer.tasks_status.values()):
         asyncio.ensure_future(consumer.send(text_data=json.dumps({
@@ -52,16 +36,45 @@ def scraper(url: str, site_id: str, consumer, event_loop):
         })), loop=event_loop)
 
 
-def run_engine(initial_links: list[dict], consumer):
+def make_safe(b64_encoded: str):
+    return b64_encoded.replace('/', '_')
+
+
+def make_b64(safe_str: str):
+    return safe_str.replace('_', '/')
+
+
+def scrape_url(browser, consumer, depth, current_depth, url, directory: Path):
+    browser.get(url)
+    html_tag = browser.find_element_by_tag_name('html')
+    html = html_tag.get_attribute('outerHTML')
+    site_id = make_safe(aes.encrypt(url))
+    filename = directory / f'{site_id}.html'
+    os.makedirs(f'{os.path.dirname(filename)}/children', exist_ok=True)
+    with open(filename, 'w') as f:
+        f.write(html)
+    consumer.tasks_status[site_id] = True
+
+    if current_depth < depth:
+        for link in html_tag.find_elements_by_tag_name('a'):
+            child_url = link.get_attribute('href')
+            children_directory = directory / 'children'
+            scrape_url(browser, consumer, depth, current_depth + 1, child_url, children_directory)
+
+
+def run_engine(data, consumer):
+    initial_links, depth = data['initial_links'], data['depth']
+    consumer.tasks_status = {}
     event_loop = asyncio.get_event_loop()
     for site in initial_links:
-        site_id = site['id']
         url = site['link']
-        asyncio.ensure_future(consumer.send(text_data=json.dumps({
-            'site_id': site_id,
-            'progress': randint(7, 19),
-        })), loop=event_loop)
-        event_loop.run_in_executor(executor, scraper, url, site_id, consumer, event_loop)
+        site_id = make_safe(aes.encrypt(url))
+        consumer.tasks_status[site_id] = False
+        # asyncio.ensure_future(consumer.send(text_data=json.dumps({
+        #     'site_id': site_id,
+        #     'progress': randint(7, 19),
+        # })), loop=event_loop)
+        event_loop.run_in_executor(executor, scraper, depth, url, consumer, event_loop)
 
     runnable_tasks = asyncio.gather(*asyncio.all_tasks(event_loop))
     asyncio.ensure_future(runnable_tasks, loop=event_loop)
