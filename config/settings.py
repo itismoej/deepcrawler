57c59e390429d7297b546bc03d5c479e3e4e5066
Mohammad Jafari <m.jafari9877@gmail.com>
2021-01-14 00:29:00 +0330

    allow all cors

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '@x08gg84$y#$ustsyi5*0vx2iim(o^&uqyos5n!38cfocc)n_8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'crawler',
     'channels',
 ]
@@ -43,6 +44,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -125,3 +127,6 @@ CHANNEL_LAYERS = {
         },
     },
 }
+
+# CORS
+CORS_ALLOW_ALL_ORIGINS = True
